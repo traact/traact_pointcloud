c0a3f611bf292591ffce0dd05c74909c6130ba28
Frieder Pankratz <frieder.pankratz@gmail.com>
2022-06-11 11:31:10 +0200

    refactor pattern and component time domain assignment

---
+++ src/traact/PointCloud.cpp
@@ -0,0 +1,5 @@
+/** Copyright (C) 2022  Frieder Pankratz <frieder.pankratz@gmail.com> **/
+#include "PointCloud.h"
+
+namespace traact {
+}// namespace traact
---
+++ src/traact/PointCloud.h
@@ -0,0 +1,13 @@
+/** Copyright (C) 2022  Frieder Pankratz <frieder.pankratz@gmail.com> **/
+
+#ifndef TRAACT_COMPONENT_POINTCLOUD_SRC_POINTCLOUD_H_
+#define TRAACT_COMPONENT_POINTCLOUD_SRC_POINTCLOUD_H_
+
+namespace traact {
+
+class PointCloud {
+};
+
+}// namespace traact
+
+#endif//TRAACT_COMPONENT_POINTCLOUD_SRC_POINTCLOUT_H_
